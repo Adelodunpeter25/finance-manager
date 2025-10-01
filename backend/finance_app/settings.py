34f7a198f87b72aa75360cc152ccc8c42429d875
Adelodunpeter25 <adelodunpeter69@gmail.com>
2025-10-01 11:18:27 +0100

    updated dockerfile

--- backend/finance_app/settings.py
+++ backend/finance_app/settings.py
@@ -24,7 +24,7 @@ load_dotenv(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default-key-for-build-only')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
